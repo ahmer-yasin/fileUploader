f6d57a9c39828c91040bff4d99b746e82b824f6d
Ahmer Yasin <ahmeryasin1992@gmail.com>
2018-06-21 01:07:41 +0500

    update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     "rules": {
         "indent": [
             "error",
-            4
+            2
         ],
         "linebreak-style": [
             "error",
@@ -20,6 +20,9 @@ module.exports = {
         "semi": [
             "error",
             "always"
+        ],
+        "console": [
+            "warning"
         ]
     }
 };
--- Server.js
+++ Server.js
@@ -1,32 +1,42 @@
 var express=require("express");
-var multer  = require('multer');
+var multer  = require("multer");
 var app = express();
 var done = false;
 
-app.use(multer({ dest: './uploads/',
- rename: function (fieldname, filename) {
+app.use(multer({ dest: "./uploads/",
+  rename: function (fieldname, filename) {
     return filename+Date.now();
   },
-onFileUploadStart: function (file) {
-  console.log(file.originalname + ' is starting ...')
-},
-onFileUploadComplete: function (file) {
-  console.log(file.fieldname + ' uploaded to  ' + file.path)
-  done=true;
-}
+  onFileUploadStart: function (file) {
+    /* eslint-disable no-alert, no-console */
+    console.log(file.originalname + " is starting ...");
+    /* eslint-enable no-alert, no-console */
+
+  },
+  onFileUploadComplete: function (file) {
+    /* eslint-disable no-alert, no-console */
+    console.log(file.fieldname + " uploaded to  " + file.path);
+    /* eslint-enable no-alert, no-console */
+
+    done=true;
+  }
 }));
 
-app.get('/',function(req,res){
-      res.sendfile("index.html");
+app.get("/",function(req,res){
+  res.sendfile("index.html");
 });
 
-app.post('/api/photo',function(req,res){
+app.post("/api/photo",function(req,res){
   if(done==true){
+    /* eslint-disable no-alert, no-console */
     console.log(req.files);
+    /* eslint-enable no-alert, no-console */
     res.end("File uploaded.");
   }
 });
 
 app.listen(3000,function(){
-    console.log("Working on port 3000");
+  /* eslint-disable no-alert, no-console */
+  console.log("Working on port 3000");
+  /* eslint-enable no-alert, no-console */
 });
