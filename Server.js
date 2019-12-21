d31c4f84e2135d60b07648259875a5f34082e744
Ahmer Yasin <ahmer@rideairlift.com>
2019-12-21 14:02:37 +0500

    resolve

--- Server.js
+++ Server.js
@@ -1,32 +1,33 @@
-var express=require("express");
-var multer  = require('multer');
+var express = require("express");
+var multer = require('multer');
 var app = express();
 var done = false;
 
-app.use(multer({ dest: './uploads/',
- rename: function (fieldname, filename) {
-    return filename+Date.now();
+app.use(multer({
+  dest: './uploads/',
+  rename: function (fieldname, filename) {
+    return filename + Date.now();
   },
-onFileUploadStart: function (file) {
-  console.log(file.originalname + ' is starting ...')
-},
-onFileUploadComplete: function (file) {
-  console.log(file.fieldname + ' uploaded to  ' + file.path)
-  done=true;
-}
+  onFileUploadStart: function (file) {
+    console.log(file.originalname + ' is starting ...')
+  },
+  onFileUploadComplete: function (file) {
+    console.log(file.fieldname + ' uploaded to  ' + file.path)
+    done = true;
+  }
 }));
 
-app.get('/',function(req,res){
-      res.sendfile("index.html");
+app.get('/', function (req, res) {
+  res.sendfile("index.html");
 });
 
-app.post('/api/photo',function(req,res){
-  if(done==true){
+app.post('/api/photo', function (req, res) {
+  if (done == true) {
     console.log(req.files);
-    res.end("File uploaded.");
+    res.send({ file: req.files });
   }
 });
-
-app.listen(3000,function(){
-    console.log("Working on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Working on port 3000");
 });
